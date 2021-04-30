9e9a31525668ad47c9e01898f64dad30d22112e9
Doodleman360 <Julislo123@gmail.com>
2021-04-29 21:17:53 -0600

    Server relocation

--- sketch.js
+++ sketch.js
@@ -1,64 +1,49 @@
-// Constants
-Y_AXIS = 1;
-X_AXIS = 2;
-let c1, c2;
+let bg;
+let SS_Duck = [];
+let potato, mouth;
+
+
+function preload() {
+    bg = loadImage('assets/background.jpeg');
+    for (let i = 0; i < 19; i++) {
+        SS_Duck[i] = loadImage('assets/duck_sheet/tile0'+i+'.png');
+    }
+
+}
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    c1 = color(204, 102, 0);
-    c2 = color(0, 102, 153);
+    createCanvas(displayWidth, displayHeight);
+    background(220);
+    image(bg, 0, 0, displayWidth, displayHeight);
+    imageMode(CENTER);
+    textSize(32);
+    fill(255);
+}
+
+let index = 4;
+let time = 0;
+let xduck = 0;
+let off = 0.0;
+function draw() {
+    time += deltaTime
+    image(bg, displayWidth/2, displayHeight/2, displayWidth, displayHeight);
+    //off = off + 0.05;
+
+    xduck = xduck + 1  * (deltaTime / 50);
+    //print(displayHeight)
+    if(xduck > width) {
+        xduck = 0;
+    }
+    image(SS_Duck[index],xduck,windowHeight-(SS_Duck[index].height/2-10));
+    if(time > 200) {
+        time = 0;
+        if (++index > 7) {
+            index = 4;
+        }
+    }
+    text("Sorry this website is currently down for server relocation", 50, 50)
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-    // Background
-    setGradient(0, 0, width, height, c2, c1, Y_AXIS);
-    fill(20,20,100);
-    makeShape(200, height - 200, 10, 0);
-    fill(0,0,100);
-    makeShape(200, height - 160, 15, 200);
-    fill(0,0,50);
-    makeShape(200, height - 120, 15, 400);
-    fill(0,0,0);
-    makeShape(200, height - 80, 15, 800);
-}
-
-function mousePressed() {
-    noiseSeed(int(random(200)));
-}
-
-function makeShape(max,yOffset, scale, offset) {
-
-    noStroke();
-    beginShape();
-    for (let i = 1; i <= max; i++) {
-        vertex(map(i, 2, max, 0, width), yOffset+(noise((i+offset)/scale)*100));
-    }
-    vertex(width, height);
-    vertex(0, height);
-    endShape(CLOSE);
-}
-
-function setGradient(x, y, w, h, c1, c2, axis ) {
-
-    noFill();
-
-    if (axis == Y_AXIS) {  // Top to bottom gradient
-        for (let i = y; i <= y+h; i++) {
-            let inter = map(i, y, y+h, 0, 1);
-            let c = lerpColor(c1, c2, inter);
-            stroke(c);
-            line(x, i, x+w, i);
-        }
-    } else if (axis == X_AXIS) {  // Left to right gradient
-        for (let i = x; i <= x+w; i++) {
-            let inter = map(i, x, x+w, 0, 1);
-            let c = lerpColor(c1, c2, inter);
-            stroke(c);
-            line(i, y, i, y+h);
-        }
-    }
 }
